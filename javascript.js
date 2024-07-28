d66d33898f84db4afbe5e970cb22acf2725eae0a
Sharongavala <143581815+Sharongavala@users.noreply.github.com>
2024-07-28 14:04:31 +0300

    added Search Engine behavior and AXIOS

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
-
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let CurrentCityElement = document.querySelector("#current-city");
+  CurrentCityElement.innerHTML = searchInput.value;
+}
+let SearchFormElement = document.querySelector("#search-form");
+SearchFormElement.addEventListener("submit", search);
